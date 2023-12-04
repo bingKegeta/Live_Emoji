298d4c57b43e2e2a825d6431ec40c4e44762efbd
Soham Ganguly <sohamg24@gmail.com>
2023-12-04 11:58:51 -0500

    implement facemesh

--- face_detect.py
+++ face_detect.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 from tkinter import Canvas
 import cv2
-import dlib
 from PIL import Image, ImageTk
 import threading
+import mediapipe as mp
 
 
 class FacialRecognitionApp:
@@ -23,18 +23,18 @@ class FacialRecognitionApp:
         self.cap = cv2.VideoCapture(0)
         self.cap.set(cv2.CAP_PROP_FPS, self.frame_rate)
 
-        # Load the Dlib face detector and facial landmark predictor.
-        self.face_detector = dlib.get_frontal_face_detector()
-        self.landmark_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+        # Initialize Mediapipe FaceMesh
+        self.mp_face_mesh = mp.solutions.face_mesh
+        self.face_mesh = self.mp_face_mesh.FaceMesh()
 
-        self.landmark_colors = {
-            "left_eye": (0, 255, 0),  # Green
-            "right_eye": (0, 255, 0),  # Green
-            "nose_bridge": (0, 0, 255),  # Blue
-            "lips": (255, 255, 0),  # Yellow
-            "jawline": (255, 0, 255),  # Magenta
-            "face_border": (0, 255, 255)  # Cyan
-        }
+        # self.landmark_colors = {
+        #     "left_eye": (0, 255, 0),  # Green
+        #     "right_eye": (0, 255, 0),  # Green
+        #     "nose_bridge": (0, 0, 255),  # Blue
+        #     "lips": (255, 255, 0),  # Yellow
+        #     "jawline": (255, 0, 255),  # Magenta
+        #     "face_border": (0, 255, 255)  # Cyan
+        # }
 
         # Start a thread for facial recognition
         self.facial_recognition_thread = threading.Thread(target=self.run_facial_recognition, daemon=True)
@@ -49,34 +49,25 @@ class FacialRecognitionApp:
 
             if ret:
                 rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-                faces = self.face_detector(rgb_frame)
 
-                for face in faces:
-                    landmarks = self.landmark_predictor(rgb_frame, face)
+                # Use Mediapipe FaceMesh to get facial landmarks
+                result = self.face_mesh.process(rgb_frame)
+                if result.multi_face_landmarks:
+                    for landmarks in result.multi_face_landmarks:
+                        # Draw lines to connect all landmarks
+                        self.draw_face_mesh(frame, landmarks)
 
-                    landmarks_data = {
-                        "frame": int(self.cap.get(cv2.CAP_PROP_POS_FRAMES)),
-                        "face_rect": (face.left(), face.top(), face.width(), face.height()),
-                        "landmarks": {
-                            # These were all manually done :(
-                            "left_eye": [(p.x, p.y) for p in landmarks.parts()[36:42]],
-                            "right_eye": [(p.x, p.y) for p in landmarks.parts()[42:48]],
-                            "nose_bridge": [(p.x, p.y) for p in landmarks.parts()[27:31]],
-                            "lips": [(p.x, p.y) for p in landmarks.parts()[48:60]],
-                            "jawline": [(p.x, p.y) for p in landmarks.parts()[0:17]],
-                            "face_border": [(p.x, p.y) for p in landmarks.parts()[17:27]],
+                        landmarks_data = {
+                            "frame": int(self.cap.get(cv2.CAP_PROP_POS_FRAMES)),
+                            "landmarks": {
+                                "face": [(p.x, p.y, p.z) for p in landmarks.landmark],
+                            }
                         }
-                    }
 
-                    # Send or process landmarks_data as needed
+                        # Send or process landmarks_data as needed
 
-                    for category, points in landmarks_data["landmarks"].items():
-                        color = self.landmark_colors.get(category, (255, 255, 255))
-                        for x, y in points:
-                            cv2.circle(frame, (x, y), 3, color, -1)
-
-                    x, y, w, h = face.left(), face.top(), face.width(), face.height()
-                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                        # Visualize landmarks on the frame
+                        self.visualize_landmarks(frame, landmarks_data)
 
                 bgr_frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
                 bgr_frame = cv2.resize(bgr_frame, (360, 320))
@@ -86,6 +77,25 @@ class FacialRecognitionApp:
                 # Update the Tkinter Canvas with the new frame
                 self.video_canvas.create_image(0, 0, anchor=tk.NW, image=img)
                 self.video_canvas.img = img
+
+    def draw_face_mesh(self, frame, landmarks):
+        for connection in mp.solutions.face_mesh.FACEMESH_CONTOURS:
+            pt1_idx = connection[0]
+            pt2_idx = connection[1]
+
+            pt1 = (int(landmarks.landmark[pt1_idx].x * frame.shape[1]),
+                   int(landmarks.landmark[pt1_idx].y * frame.shape[0]))
+            pt2 = (int(landmarks.landmark[pt2_idx].x * frame.shape[1]),
+                   int(landmarks.landmark[pt2_idx].y * frame.shape[0]))
+
+            cv2.line(frame, pt1, pt2, (0, 255, 0), 2)
+
+    def visualize_landmarks(self, frame, landmarks_data):
+        for point in landmarks_data["landmarks"]["face"]:
+            x, y, z = point
+            x = int(x * frame.shape[1])
+            y = int(y * frame.shape[0])
+            cv2.circle(frame, (x, y), 3, (255, 0, 0), -1)
 
     def on_closing(self):
         self.cap.release()
